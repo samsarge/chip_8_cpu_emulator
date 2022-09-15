9c90dab3aaf480ae471b703f4bb2cc19f6e203e7
samsarge <samsarge@hotmail.co.uk>
2022-09-15 17:48:50 +0100

    Add support for functions with CALL and RETURN operations Implement a stack to support CALL and RETURN operations Use the new support for opcodes to calculate  5 + (10 * 2) + (10 * 2)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@
 // filter out the bits we dont care about to access each individual nibble
 
 // CHIP-8 breaks opcodes down into 2 bytes, high byte, low byte, then nibbles, high nibble, low nibble
-
+// (layout of the bytes is called endianness, cpu manufacturers decide this)
 // 0xAB12
 // high byte = AB
   // high nibble A
@@ -16,6 +16,11 @@
 // low byte = 12
   // high nibble 1
   // low nibble 2
+
+// Note that control flow in a CPU is done by comparing values in a register
+// then modifying position_in_memory, depending on the outcome. There are no while
+// or for loops in the CPU, thats the job of the programming languages compiler.
+use core::panic;
 
 // All CHIP-8 opcodes are U16 values, defined by who makes the architecture
 struct CPU {
@@ -27,7 +32,11 @@ struct CPU {
     // 0x1000 is hex for 4096 (4kb), the amount of bytes of RAM a CHIP-8 had.
     // The chip-8 usize equiv basically, only 2^12 (12 bits = 4096)
     // In original spec, the first 512 bytes (0x100) are reserved for the system, others are for programs
-    memory: [u8; 0x1000]
+    memory: [u8; 0x1000],
+
+    // ~ The stack ~ specialised memory for CALL and RETURN opcodes
+    stack: [u16; 16], // stacks maximum height is 16m after 16 nested function calls we say its a stack overflow
+    stack_pointer: usize // giving the stack_pointer usize makes it easier to index values cause rust
 }
 
 impl CPU {
@@ -71,18 +80,24 @@ impl CPU {
 
             // You can select multiple nibbles by increasing the width of the filter.
             // we dont need to bit shift them cause they're already in lowest significant place
-            // let nnn = opcode & 0x0FFF;
-            // let kk = opcode & 0x00FF;
+
+            // To support functions
+            let nnn = opcode & 0x0FFF;
+            let kk = opcode & 0x00FF;
+
+            // 
 
             match (c, x, y, d) {
                 (0, 0, 0, 0) => { return; }, // terminate execution when opcode 0x0000 is encountered
-                (0x8, _, _, 0x4) => self.add_xy(x, y), // 8 - uses 2 registers, 4 - addition
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode) // add more functionality
             }
         }
     }
 
-    // Add y to x register
+    // ADD_XY: Add y to x register
     fn add_xy(&mut self, x: u8, y: u8) {
         let arg1 = self.registers[x as usize];
         let arg2 = self.registers[y as usize];
@@ -100,8 +115,40 @@ impl CPU {
         }
     }
 
-}
+    // CALL: opcode 0x2nnn sets position_in_memory to nnn, the address of the function
+    // Each CALL opcode adds an address to the stack by incrementing the stack pointer
+    // and writing nnn to that position in the stack.
 
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;   
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow");
+        }
+
+        // add current position in memory to stack
+        // memory address is two bytes higher than calling location as it is incremented within the body of run()
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+
+        // modify position in memory to affect jumping to that address
+        self.position_in_memory = addr as usize;
+    }
+
+    // RETURN: opcode 0x00EE sets position_in_memory to the memory address of the previous CALL opcode
+    // Each RETURN opcode removes the top address by decrementing the stack pointer.
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        // jump to position in memory where an earlier call was made
+        let call_addr = self.stack[self.stack_pointer];
+        self.position_in_memory = call_addr as usize;
+    }
+}
 
 fn main() {
     // Values are held in the registers
@@ -110,26 +157,38 @@ fn main() {
         // repeat expressions [x; N], which produces an array with N copies of x
         registers: [0; 16],
         memory: [0; 4096],
-        position_in_memory: 0
+        position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0
     };
+
+    // Use our CPU to calculate: 5 + (10 * 2) + (10 * 2) = 45
 
     // load some data into our registers for processing
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
 
-    // load instructions into memory
-    mem[0] = 0x80; mem[1] = 0x14; // load opcode 0x8014, which adds register 1 to register 0
-    mem[2] = 0x80; mem[3] = 0x24; // load opcode 0x8024, which adds register 2 to register 0
-    mem[4] = 0x80; mem[5] = 0x34; // load opcode 0x8034, which adds register 3 to register 0
+    // set opcode to 0x2100: CALL the function at 0x100
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    // set opcode to 0x2100: CALL the function at 0x100
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    // sets opcode to 0x0000: HALT (not really needed as cpu.memory is initialized with null bytes)
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
+
+
+    // sets opcode to 0x8014: ADD register 1s value to register 0
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    // sets opcode to 0x8014: ADD register 1s value to register 0
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    // sets opcode to 0x00EE: RETURN
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
 
     // execute main cpu loop
     cpu.run();
 
-    // 5 + 10 + 10 + 10
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
+
 }
 
